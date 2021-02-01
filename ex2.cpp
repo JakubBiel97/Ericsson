c0bb8191365748b2846d696b177d49a03a81a957
Jakub Biel <jakubbiel97@gmail.com>
2021-02-01 22:31:34 +0100

    Creating body of our program

--- ex2.cpp
+++ ex2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+void deciToBina();
+void binaToDeci();
+
+int main()
+{
+    int decNumber = 0;
+
+
+
+}
+
+void deciToBina()
+{
+
+}
+
+void binaToDeci()
+{
+
+}
