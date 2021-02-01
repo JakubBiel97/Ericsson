4a7db3aac5fc8449ae0867823d1d4e5d9dfa1d18
Jakub Biel <jakubbiel97@gmail.com>
2021-02-01 21:46:11 +0100

    ErrorCounter

--- ex1.cpp
+++ ex1.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
+    int gettingTheStringLength = 0;
     int numberOfLoadedObjects = 0;
     int numberOfErrorObjects = 0;
 
-    //char* numbers[];
+    string endingString;
     string numbersFromInput;
+    char tempChar;
     fstream file;
     file.open("input.txt", ios::in);                        //openning the the input file
 
@@ -27,10 +30,40 @@ int main()
     }
 
     unsigned int strLen = numbersFromInput.length();            //ilosc znakow
-    //ilosc wczytanych obiektow:
+    //number of objects loaded
     numberOfLoadedObjects = strLen / 8;                         //8 bitow to 1 obiekt
-    cout << numberOfLoadedObjects;
+    cout << numberOfLoadedObjects << endl;
 
-    //for (int i = 0; i < strLen; i + 8)
+    //swipping around our string for errors
+    for (int i = 0; i < strLen; i+=8)
+    {
+        //looking for errors in number and control bite
+        if(numbersFromInput[i+3] != '0' && numbersFromInput[i+7] == '0')
+        {
+            numberOfErrorObjects++;
+            cout << i << endl;
+        }
+        else if(numbersFromInput[i+3] != '1' && numbersFromInput[i+7] == '1')
+        {
+            numberOfErrorObjects++;
+            cout << i << endl;
+        }
+        else if(numbersFromInput[i+4] == '0' && numbersFromInput[i+5] == '0'
+                && numbersFromInput[i+6] == '0')
+        {
+            numberOfErrorObjects++;
+            cout << i << endl;
+        }
+        else
+        {
+            cout << i << endl;
+        }
+/*        else
+        {
+            for(int j = i; j < strLen;)
+        }*/
 
+    }
+
+    cout << "Ilosc bledow: " << numberOfErrorObjects;
 }
