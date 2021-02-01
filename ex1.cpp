5a9f2d2d4d823ab141b494c2d34fe124a8dbd88e
Jakub Biel <jakubbiel97@gmail.com>
2021-02-01 20:24:44 +0100

    Opening file and getting our input

--- ex1.cpp
+++ ex1.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    for(int i = 0; i < 10; i++)
+    int numberOfLoadedObjects = 0;
+    int numberOfErrorObjects = 0;
+
+    //char* numbers[];
+    string numbersFromInput;
+    fstream file;
+    file.open("input.txt", ios::in);        //openning the the input file
+
+    if(file.good() == false)                //testing if file did open
     {
-        cout << "cos" << endl;
+        cout << "Error while opening the file." << endl;
+        exit(0);
     }
+    else
+    {
+        getline(file, numbersFromInput);    //getting the numbers to our string
+        file.close();                       //closing file
+        //cout << numbersFromInput << endl;
+    }
+
+
+
 }
