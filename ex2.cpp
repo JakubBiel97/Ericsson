e90a393cd3b057c6f5f223008687fe638034587f
Jakub Biel <jakubbiel97@gmail.com>
2021-02-02 00:59:11 +0100

    Full Dec to binary done

--- ex2.cpp
+++ ex2.cpp
@@ -3,73 +3,112 @@
 
 using namespace std;
 
-void deciToBina();
-void binaToDeci();
+void deciToBina(int );
+void binaToDeci(string , int);
 string binNumber;
 int decNumber = 0;
+int strLen = 0;
 int temp = 1;
 
 int main()
 {
-    int choise;
-    cout << "Hello, welcome in Jacob's calculator." << endl;
-    cout << "If you would like to change Decimal to a Binary number - press 1." << endl;
-    cout << "If you would like to change Binary to a Decimal number - press 2." << endl;
-    cin >> choise;
-
-    while(choise != 2 && choise != 1)                                   //safety kit
+    for(;;)
     {
-        cout << "Wrong number try again! ( 1 or 2).";
+        int choise;
+        cout << "Hello, welcome in Jacob's calculator." << endl;
+        cout << "If you would like to change Decimal to a Binary number - press 1." << endl;
+        cout << "If you would like to change Binary to a Decimal number - press 2." << endl;
         cin >> choise;
-    }
 
-    if(choise == 1)
-    {
-        deciToBina();
-    }
-    else
-    {
-        binaToDeci();
+        while(choise != 2 && choise != 1)                                   //safety kit
+        {
+            cout << "Wrong number try again!(1 or 2).";
+            cin >> choise;
+        }
+
+        if(choise == 1)
+        {
+            cout << "Enter a decimal number(max: 32767): ";
+            cin >> decNumber;
+            cout << decNumber << " in binary system is: ";
+            deciToBina(decNumber);
+            cout <<"." << endl;
+        }
+        else
+        {                                               //binary to deci
+            cout << "Enter a binar number: " << endl;
+            cin >> binNumber;
+
+            while(binNumber[strLen] != NULL)           //getting our string length
+            {
+                strLen++;
+            }
+
+            //cout << endl << strLen;
+
+            /*for(int i = 0; i < strLen; i++)                       //safety first
+            {
+                while(binNumber[i] != '0' || binNumber[i] != '1' || binNumber[i] != NULL)
+                {
+                    cout << binNumber << " is not a binary number, try again:" << endl;
+                    cin >> binNumber;
+                    i = 0;
+                    while(binNumber[strLen] != NULL)           //getting our string length
+                    {
+                        strLen++;
+                    }
+                }
+            }*/
+            binaToDeci(binNumber, strLen);
+        }
+        cout << endl << endl;
     }
 }
 
-void deciToBina()
+void deciToBina(int decNumber)
 {
+    int x = 0;
+    int temp[15];
 
+    for(int i = 0; decNumber > 0; i++)
+    {
+        temp[i] = decNumber % 2;
+        decNumber = decNumber / 2;
+        x++;
+    }
+
+    for(int i = x - 1; i >= 0; i--)
+    {
+        cout << temp[i];
+    }
 }
 
-void binaToDeci()
+void binaToDeci(string binNumber, int stringLength)
 {
-    int strLen = 0;
-
-    cout << "Type binary number: " << endl;
-    cin >> binNumber;
-
     while(binNumber[strLen] != NULL)           //getting our string length
     {
         strLen++;
     }
 
-    for(int i = 0; i < strLen; i++)                       //safety first
+    /*for(int i = 0; i < strLen; i++)                       //safety first
     {
-        if(binNumber[i] != '0')
+        while(binNumber[i] != '0' || binNumber[i] != '1')
         {
-            cout << "Wrong number, try again." << endl;
-            exit(0);
-        }
-        else if(binNumber[i] != '1')
-        {
-            cout << "Wrong number, try again." << endl;
-            exit(0);
-        }
-        else if(binNumber[i] != NULL)
-        {
-            cout << "Wrong number, try again." << endl;
-            exit(0);
-        }
-    }
+            cout << binNumber << " is not a binary number, try again:" << endl;
+            cin >> binNumber;
+            i = 0;
 
-    for(int i = strLen; i < 0; i--)
+            while(binNumber[strLen] != NULL)           //getting our string length
+            {
+                strLen++;
+            }
+        }
+    }*/
+    cout << endl << stringLength << endl;
+
+    cout << binNumber[1] << binNumber[2] << endl;
+
+    for(int i = stringLength - 1; i >= 0; i--)
     {
         if(binNumber[i] == '1')
         {
@@ -77,6 +116,7 @@ void binaToDeci()
         }
 
         temp *= 2;
+        cout << endl << temp << endl;
     }
 
     cout << endl << binNumber << " in Decimal will be: " << decNumber << endl;
