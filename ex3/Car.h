fe201f31473b6f12ad886c9a724bfcc434bb9499
Jakub Biel <jakubbiel97@gmail.com>
2021-02-02 11:57:40 +0100

    Adding header file for our class

--- ex3/Car.h
+++ ex3/Car.h
@@ -1,4 +1,19 @@
+#pragma once
 #include <iostream>
 
 using namespace std;
+
+class Car
+{
+private:
+	int speed = 0;
+	int engineTemp = 0;
+	int outsideTemp = 0;
+
+public:
+	Car(int, int, int);
+	~Car();
+
+	void go();
+};
 
