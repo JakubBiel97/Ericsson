99046eec79c759cce5726a0e9111694e4dea6aa8
Jakub Biel <jakubbiel97@gmail.com>
2021-02-02 11:07:13 +0100

    AAdding some random speed changes

--- ex3/Car.cpp
+++ ex3/Car.cpp
@@ -1,3 +1,34 @@
 #include "Car.h"
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
+
+void Car::go()
+{
+	for (;;)													//infinite loop
+	{
+		srand(time(NULL));
+		int tempTime = 0;										//will store there 1 or 0 - (adding speed or lowering the speed) fully randomly
+		tempTime = rand() % 2;
+		
+		cout << "Predkosc == " << this->speed;
+		_sleep(1000);
+		if (tempTime == 1)
+		{
+			this->speed += rand() % 3;							//changing the speed about 0,1 or 2 hm\h
+		}
+		else
+		{
+			this->speed -= rand() % 3;
+			if (this->speed < 0)
+			{
+				this->speed = 0;
+				cout << "Car has stopped.";
+				exit(0);
+			}
+		}
+		
+
+	}
+}
